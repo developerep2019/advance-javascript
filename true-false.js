e7e37c7db4b1f887d8d8e340ca3d5a4b12bdbaa3
developerep2019 <developerep2019@gmail.com>
2020-08-16 02:00:45 +0600

    true-false done

---
+++ true-false.js
@@ -0,0 +1,26 @@
+/*
+False type values :
+0
+''
+undefined
+NaN
+false
+Truthy Values :
+'0'
+' '
+[]
+*/
+
+
+
+
+const age = 4;
+// const name = ' ';
+let name = 'false';
+console.log(name);
+if(name) {
+    console.log('Condition is true')
+}
+else{
+    console.log('Condition is false')
+}
